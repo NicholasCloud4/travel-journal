22ebd76d5b48be4688b901bf8d6feb60cdfd7f7e
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-12-03 12:22:44 -0500

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header.css'
+
+export default function Header() {
+    return (
+        <header>
+            <img src="src/assets/globe.png" alt="globe" className='globe' />
+            <h3>My Travel Journal</h3>
+        </header>
+    )
+}
